45388b5879a1583634a0875b30d88af142eb30df
HydraDE <30797458+HydraDE@users.noreply.github.com>
2025-09-15 16:54:24 +0200

    Updated build.gradle and .gitignore Updated WrapperPlayServerEntityDestroy, WrapperPlayServerEntityMetadata and WrapperPlayServerGameStateChange

--- src/main/java/com/comphenix/packetwrapper/WrapperPlayServerEntityMetadata.java
+++ src/main/java/com/comphenix/packetwrapper/WrapperPlayServerEntityMetadata.java
@@ -1,101 +1,107 @@
 /**
- * PacketWrapper - ProtocolLib wrappers for Minecraft packets
- * Copyright (C) dmulloy2 <http://dmulloy2.net>
- * Copyright (C) Kristian S. Strangeland
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * PacketWrapper - ProtocolLib wrappers for Minecraft packets Copyright (C) dmulloy2
+ * <http://dmulloy2.net> Copyright (C) Kristian S. Strangeland
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License along with this program.  If
+ * not, see <http://www.gnu.org/licenses/>.
  */
 package com.comphenix.packetwrapper;
-
-import java.util.List;
-
-import org.bukkit.World;
-import org.bukkit.entity.Entity;
 
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
-import com.comphenix.protocol.wrappers.WrappedWatchableObject;
+import com.comphenix.protocol.wrappers.WrappedDataValue;
+import com.comphenix.protocol.wrappers.WrappedDataWatcher.Serializer;
+import java.util.List;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
 
 public class WrapperPlayServerEntityMetadata extends AbstractPacket {
-	public static final PacketType TYPE =
-			PacketType.Play.Server.ENTITY_METADATA;
 
-	public WrapperPlayServerEntityMetadata() {
-		super(new PacketContainer(TYPE), TYPE);
-		handle.getModifier().writeDefaults();
-	}
+  public static final PacketType TYPE =
+      PacketType.Play.Server.ENTITY_METADATA;
 
-	public WrapperPlayServerEntityMetadata(PacketContainer packet) {
-		super(packet, TYPE);
-	}
+  public WrapperPlayServerEntityMetadata() {
+    super(new PacketContainer(TYPE), TYPE);
+    handle.getModifier().writeDefaults();
+  }
 
-	/**
-	 * Retrieve Entity ID.
-	 * <p>
-	 * Notes: entity's ID
-	 * 
-	 * @return The current Entity ID
-	 */
-	public int getEntityID() {
-		return handle.getIntegers().read(0);
-	}
+  public WrapperPlayServerEntityMetadata(PacketContainer packet) {
+    super(packet, TYPE);
+  }
 
-	/**
-	 * Set Entity ID.
-	 * 
-	 * @param value - new value.
-	 */
-	public void setEntityID(int value) {
-		handle.getIntegers().write(0, value);
-	}
+  /**
+   * Retrieve Entity ID.
+   * <p>
+   * Notes: entity's ID
+   *
+   * @return The current Entity ID
+   */
+  public int getEntityID() {
+    return handle.getIntegers().read(0);
+  }
 
-	/**
-	 * Retrieve the entity of the painting that will be spawned.
-	 * 
-	 * @param world - the current world of the entity.
-	 * @return The spawned entity.
-	 */
-	public Entity getEntity(World world) {
-		return handle.getEntityModifier(world).read(0);
-	}
+  /**
+   * Set Entity ID.
+   *
+   * @param value - new value.
+   */
+  public void setEntityID(int value) {
+    handle.getIntegers().write(0, value);
+  }
 
-	/**
-	 * Retrieve the entity of the painting that will be spawned.
-	 * 
-	 * @param event - the packet event.
-	 * @return The spawned entity.
-	 */
-	public Entity getEntity(PacketEvent event) {
-		return getEntity(event.getPlayer().getWorld());
-	}
+  /**
+   * Retrieve the entity of the painting that will be spawned.
+   *
+   * @param world - the current world of the entity.
+   * @return The spawned entity.
+   */
+  public Entity getEntity(World world) {
+    return handle.getEntityModifier(world).read(0);
+  }
 
-	/**
-	 * Retrieve Metadata.
-	 * 
-	 * @return The current Metadata
-	 */
-	public List<WrappedWatchableObject> getMetadata() {
-		return handle.getWatchableCollectionModifier().read(0);
-	}
+  /**
+   * Retrieve the entity of the painting that will be spawned.
+   *
+   * @param event - the packet event.
+   * @return The spawned entity.
+   */
+  public Entity getEntity(PacketEvent event) {
+    return getEntity(event.getPlayer().getWorld());
+  }
 
-	/**
-	 * Set Metadata.
-	 * 
-	 * @param value - new value.
-	 */
-	public void setMetadata(List<WrappedWatchableObject> value) {
-		handle.getWatchableCollectionModifier().write(0, value);
-	}
+  /**
+   * Retrieve Metadata.
+   *
+   * @return The current Metadata
+   */
+  public List<WrappedDataValue> getMetadata() {
+    return handle.getDataValueCollectionModifier().read(0);
+  }
+
+  /**
+   * Set Metadata.
+   *
+   * @param values - new values.
+   */
+  public void setMetadata(List<WrappedDataValue> values) {
+    handle.getDataValueCollectionModifier().write(0, values);
+  }
+
+  public void setIndex(int index, Object value, Serializer serializer) {
+    List<WrappedDataValue> values = this.getMetadata();
+
+    values.removeIf(wrappedDataValue -> wrappedDataValue.getIndex() == index);
+    values.add(new WrappedDataValue(index, serializer, value));
+
+    this.setMetadata(values);
+  }
 }
